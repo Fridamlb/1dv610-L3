862ea89bbb4c150d8d4c5a95ebdb1992d233963f
Frida Berg <fb223gw@student.lnu.se>
2026-04-23 19:03:03 +0200

    Installed modulen och ändrade importen

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
-import DocumentStats from './L2-module/src/DocumentStats.js'
+import DocumentStats from 'document-stats-analyzer'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
